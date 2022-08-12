a8aee0dd66c0a97cd43285bc4d7951d8ed2f1876
Evgeniy <evgeniykovalsky04386@gmail.com>
2022-08-12 21:52:07 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React from 'react';
+
+export default class  App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+        description:false
+    };
+
+   
 }
 
-export default App;
+   render(){
+    return(
+    <div className="conteiner">
+        <div className="head">{this.props.data.Name +'   '+ this.props.data.SurName}</div>
+        <div className="mainBody">
+            <div className="picture"> <img src={this.props.data.image} alt={this.props.data.id}/></div>
+            <div className="description">
+                <p><span>Имя:</span> {this.props.data.Name}</p>
+                <p><span>Фамилия:</span> {this.props.data.SurName}</p>
+                <p><span>Возраст:</span> {this.props.data.fullYearsOld}</p>
+                <p><span>Семейное положение:</span> {this.props.data.family}</p>
+                <p><span>Cтрана:</span> {this.props.data.country}</p>
+                <p><span>Тел.:</span> {this.props.data.tell}</p>
+                <p><span>Email:</span> {this.props.data.email}</p>
+                <p className="lsDesc">{this.props.data.description}</p>
+               
+            </div>
+        </div>
+        <div className="footer">
+        <button 
+            onClick={()=>this.setState({description:this.state.description?false:true})}
+            >{this.state.description? 'Cкрыть':'Подробнее..'}</button>
+            {this.state.description && <p>{this.props.data.allDescription}</p>}
+        </div>
+        
+     </div>
+  
+    )
+   }
+}
+
+
